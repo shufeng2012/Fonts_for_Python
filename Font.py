1abc8c5722e47262fbe048df694a748a01964420
shufeng2012 <160096906+shufeng2012@users.noreply.github.com>
2025-09-13 20:38:23 +0800

    Update Font.py

--- Font.py
+++ Font.py
@@ -2,7 +2,6 @@
 # coding = UTF-8
 '''
 一个可以自由操控字体的Python库
-from: shufeng2012
 '''
 
 class Font:
@@ -17,7 +16,7 @@ class Font:
         BLACK: str = "30"
         RED: str = "31"
         GREEN: str = "32"
-        BROWN: str = "33"
+        YELLOW: str = "33"
         BLUE: str = "34"
         PURPLE: str = "35"
         CYAN: str = "36"
@@ -28,7 +27,7 @@ class Font:
         BLACK: str = "40"
         RED: str = "41"
         GREEN: str = "42"
-        BROWN: str = "43"
+        YELLOW: str = "43"
         BLUE: str = "44"
         PURPLE: str = "45"
         CYAN: str = "46"
@@ -36,8 +35,8 @@ class Font:
 
     class Variant:
         # 字体变体
-        HIGHLIGHT: str = "1"
-        LOWLIGHT: str = "2"
+        BOLD: str = "1"
+        DARKEN: str = "2"
         ITALIC: str = "3"
         UNDERLINE: str = "4"
         FLASHING: str = "5"
@@ -66,17 +65,17 @@ class Font:
 
         return return_string
 
-    def variant(self, string: str, highlight: bool = False, lowlight: bool = False, italic: bool = False, underline: bool = False, flashing: bool = False, reverse_display: bool = False, blanking: bool = False, reset: bool = True) -> str:
+    def variant(self, string: str, bold: bool = False, darken: bool = False, italic: bool = False, underline: bool = False, flashing: bool = False, reverse_display: bool = False, blanking: bool = False, reset: bool = True) -> str:
         '''
         处理字符串变体
         '''
         return_string: str = string     # 返回处理后的字符串
         variant: str
 
-        if highlight == True:               # 高亮
-            variant = f"\033[{self.Variant.HIGHLIGHT}m"
-        elif lowlight == True:              # 低亮
-            variant = f"\033[{self.Variant.LOWLIGHT}m"
+        if bold == True:               # 高亮
+            variant = f"\033[{self.Variant.BOLD}m"
+        elif darken == True:              # 低亮
+            variant = f"\033[{self.Variant.DARKEN}m"
         elif italic == True:                # 斜体
             variant = f"\033[{self.Variant.ITALIC}m"
         elif underline == True:             # 下划线
@@ -95,31 +94,31 @@ class Font:
 
         return return_string
 
-    def __init__(self, string: str = None, Fore: str = "", Back: str = None, highlight: bool = False, lowlight: bool = False, italic: bool = False, underline: bool = False, flashing: bool = False, reverse_display: bool = False, blanking: bool = False, reset: bool = True) -> None:
+    def __init__(self, string: str = None, Fore: str = "", Back: str = None, bold: bool = False, darken: bool = False, italic: bool = False, underline: bool = False, flashing: bool = False, reverse_display: bool = False, blanking: bool = False, reset: bool = True) -> None:
         '''
         构造函数
         '''
         return_string: str          # 返回的处理后的字符串
         ansi: str                   # ANSI转义序列
 
-        if highlight == True:               # 高亮
-            ansi = f"\033[{self.Variant.HIGHLIGHT};"
+        if bold == True:               # 高亮
+            ansi = f"\033[{self.Variant.BOLD};"
             if Fore != "":           # 不为空
                 if Back == None:            # 无背景
                     ansi += f"{Fore}m"
                 else:                       # 有背景
-                    ansi = f"\033[{self.Variant.HIGHLIGHT};{Back}m\033[{self.Variant.HIGHLIGHT};{Fore}m"
+                    ansi = f"\033[{self.Variant.BOLD};{Back}m\033[{self.Variant.BOLD};{Fore}m"
             else:                           # 为空
-                ansi = f"\033[{self.Variant.HIGHLIGHT};{Back}m"
-        elif lowlight == True:               # 低亮
-            ansi = f"\033[{self.Variant.LOWLIGHT};"
+                ansi = f"\033[{self.Variant.BOLD};{Back}m"
+        elif darken == True:               # 低亮
+            ansi = f"\033[{self.Variant.DARKEN};"
             if Fore != "":           # 不为空
                 if Back == None:            # 无背景
                     ansi += f"{Fore}m"
                 else:                       # 有背景
-                    ansi = f"\033[{self.Variant.LOWLIGHT};{Back}m\033[{self.Variant.LOWLIGHT};{Fore}m"
+                    ansi = f"\033[{self.Variant.DARKEN};{Back}m\033[{self.Variant.DARKEN};{Fore}m"
             else:                           # 为空
-                ansi = f"\033[{self.Variant.LOWLIGHT};{Back}m"
+                ansi = f"\033[{self.Variant.DARKEN};{Back}m"
         elif italic == True:               # 斜体
             ansi = f"\033[{self.Variant.ITALIC};"
             if Fore != "":           # 不为空
@@ -176,6 +175,5 @@ class Font:
         return_string = ansi + string
         if reset == True:                   # 是否重置
             return_string += self.STOP
-
 
         print(return_string)
